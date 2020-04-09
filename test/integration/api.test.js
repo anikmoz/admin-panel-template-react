7a06b732ee8e36d85fec9824946c69bf9433df96
Anik <anik.airworld@gmail.com>
2020-04-09 20:03:10 +0600

    updated packages

--- test/integration/api.test.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest');
-const app = require('../../app');
-
-describe('index page', () => {
-  it('should load', (done) => {
-    request(app).get('/').expect(200, done);
-  });
-});
